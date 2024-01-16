b5f23abd2f46af4971d77acb247649fb79823163
matlin <cymatlin@gmail.com>
2024-01-16 14:27:53 +0200

    may Portfolio all workes

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('mouseenter', () => {
+            // Add animation or interaction
+            card.style.boxShadow = '0 4px 8px 0 rgba(0,0,0,0.2)';
+        });
+        card.addEventListener('mouseleave', () => {
+            // Remove animation or interaction
+            card.style.boxShadow = 'none';
+        });
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function(e) {
+                e.preventDefault();
+        
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+        
+    });
+});
+
+// Close the modal if the user clicks anywhere outside of it
+window.onclick = function(event) {
+    let modal = document.getElementById("resumeModal");
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
+function openModal() {
+    let modal = document.getElementById("resumeModal");
+    let modalContent = modal.querySelector('.modal-content');
+
+    modal.style.display = "block";
+    modal.style.animation = 'fadeIn 0.3s'; // Apply fadeIn animation
+    modal.offsetHeight; // Trigger reflow to restart animation
+    modal.style.animation = null;
+
+    modalContent.style.animation = 'popIn 0.3s'; // Apply popIn animation
+    modalContent.offsetHeight; // Trigger reflow to restart animation
+    modalContent.style.animation = null;
+}
+
+function closeModal() {
+    let modal = document.getElementById("resumeModal");
+    let modalContent = modal.querySelector('.modal-content');
+
+    modal.style.animation = 'fadeOut 0.3s'; // Apply fadeOut animation
+    modal.offsetHeight; // Trigger reflow to restart animation
+
+    modal.style.display = "none";
+    modal.style.animation = null;
+
+    modalContent.style.animation = 'popOut 0.3s'; // Apply popOut animation
+    modalContent.offsetHeight; // Trigger reflow to restart animation
+    modalContent.style.animation = null;
+}
+
